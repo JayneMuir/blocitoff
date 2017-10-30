4a60eef358817a0e57c90b4d6ca4728bb5a2999a
Jayne Muir <jaynemuir2017@gmail.com>
2017-10-29 21:10:32 -0700

    checkpoint-2-firebase: syncing with firebase

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,8 +1,34 @@
 (function() {
-    function HomeCtrl() {
-    }
+    function HomeCtrl($scope, $firebaseArray) 
+    {
+        this.taskListTitle = "To Do List";
+        
+        var ref = blocitoffDB.database().ref();
 
+        var tasks = $firebaseArray(ref);
+        
+        // make the tasks available in the DOM
+        $scope.tasks = tasks;
+        
+        // add a task
+        $scope.addTask = function () {
+            var todaysDateTime = new Date();
+            var newTask = {};
+            newTask.Description = $scope.newTaskDescription;
+            newTask.InsertedDate = todaysDateTime.toDateString();
+            tasks.$add(newTask).then(function(ref) {
+                console.log("added record with description " + newTask.Description);
+            });
+        };
+        
+        // remove task
+	   $scope.removeTask = function (task) {
+            tasks.$remove(task).then(function(ref) {
+                console.log("removed record with description " + task.Description);
+            });
+        };
+    }
     angular
         .module('blocitoff')
-        .controller('HomeCtrl', [HomeCtrl]);
+        .controller('HomeCtrl', ["$scope", "$firebaseArray", HomeCtrl]);
 })();
