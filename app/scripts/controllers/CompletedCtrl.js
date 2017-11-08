8ce1fa5e5780fc834d0407fb34320f4fcdfe8314
Jayne Muir <jaynemuir2017@gmail.com>
2017-11-07 21:33:19 -0800

    Tried orderby priority

--- app/scripts/controllers/CompletedCtrl.js
+++ app/scripts/controllers/CompletedCtrl.js
@@ -6,6 +6,10 @@
         FirebaseService.load();
         this.tasks = FirebaseService.getTasks();
         $scope.tasks = this.tasks;
+        // remove task
+	    $scope.removeTask = function (task) {
+           FirebaseService.removeTask(task);
+        };
     }
     angular
         .module('blocitoff')
